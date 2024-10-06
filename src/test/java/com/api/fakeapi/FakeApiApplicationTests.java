99c435e5b049afc54d4e5a6db9957b90b12ded79
nicorugedeveloper <nicolasruge.13@gmail.com>
2024-10-05 23:40:48 -0500

    feat: initialize project with base configuration and dependencies

---
+++ src/test/java/com/api/fakeapi/FakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.fakeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
